d06f4a97962dd4ce564979430a44c37339096b0d
Clemens Elflein <clemens1@familie-elflein.de>
2025-03-24 19:48:41 +0100

    Update PHY reset routine to fix issues during power up

--- bootloader/boards/XCORE/board_ex.h
+++ bootloader/boards/XCORE/board_ex.h
@@ -2,48 +2,57 @@
 #define BOARD_EX_H
 
 #define BOARD_PHY_ADDRESS 31
-#define BOARD_PHY_RESET()                      \
-  do {                                         \
-    uint8_t tries = 0;                         \
-    uint32_t i = 0;                            \
-    while (1) {                                \
-      mii_write(&ETHD1, 0x1f, 0xFA00);         \
-      uint32_t value = mii_read(&ETHD1, 0x00); \
-      if (value & 0x1) {                       \
-        break;                                 \
-      }                                        \
-      i = STM32_SYS_CK / 1000;                 \
-      while (i-- > 0) {                        \
-        asm("nop");                            \
-      };                                       \
-      tries++;                                 \
-      if (tries == 100) {                      \
-        mii_write(&ETHD1, 0x1F, 0xFA00);       \
-        mii_write(&ETHD1, 0x01, 0xFFFF);       \
-        i = STM32_SYS_CK / 100;                \
-        while (i-- > 0) {                      \
-          asm("nop");                          \
-        };                                     \
-      }                                        \
-      if (tries > 200) {                       \
-        palClearLine(LINE_RESET_PHY);          \
-        i = STM32_SYS_CK / 100;                \
-        while (i-- > 0) {                      \
-          asm("nop");                          \
-        };                                     \
-        palSetLine(LINE_RESET_PHY);            \
-        tries = 0;                             \
-      }                                        \
-    }                                          \
-    i = STM32_SYS_CK / 100;                    \
-    while (i-- > 0) {                          \
-      asm("nop");                              \
-    };                                         \
-                                               \
-    mii_write(&ETHD1, 0x1F, 0xF100);           \
-    mii_write(&ETHD1, 0x00, 0x40B3);           \
-    mii_write(&ETHD1, 0x1F, 0xF410);           \
-    mii_write(&ETHD1, 0x00, 0x2A05);           \
+#define BOARD_PHY_RESET()                                               \
+  do {                                                                  \
+    uint32_t i;                                                         \
+    bool valid = true;                                                  \
+    palSetLine(LINE_HEARTBEAT_LED_RED);                                 \
+                                                                        \
+    do {                                                                \
+      bool was_hardware_reset = READ_REG(RCC->RSR) & (RCC_RSR_PORRSTF); \
+      if (was_hardware_reset || !valid) {                               \
+        i = STM32_SYS_CK / 100;                                         \
+        while (i-- > 0) {                                               \
+          asm("nop");                                                   \
+        }                                                               \
+                                                                        \
+        palToggleLine(LINE_HEARTBEAT_LED_RED);                          \
+        palClearLine(LINE_RESET_PHY);                                   \
+        i = STM32_SYS_CK / 20;                                          \
+        while (i-- > 0) {                                               \
+          asm("nop");                                                   \
+        }                                                               \
+        palSetLine(LINE_RESET_PHY);                                     \
+        i = STM32_SYS_CK / 20;                                          \
+        while (i-- > 0) {                                               \
+          asm("nop");                                                   \
+        }                                                               \
+        mii_write(&ETHD1, 0x1F, 0xF100);                                \
+        mii_write(&ETHD1, 0x00, 0x40B3);                                \
+        mii_write(&ETHD1, 0x1F, 0xF410);                                \
+        mii_write(&ETHD1, 0x00, 0x2A05);                                \
+      }                                                                 \
+                                                                        \
+      valid = true;                                                     \
+      mii_write(&ETHD1, 0x1f, 0xFA00);                                  \
+      uint32_t value = mii_read(&ETHD1, 0x00);                          \
+      bool is_initialized = value & 0x1;                                \
+      valid &= is_initialized;                                          \
+      if (valid) {                                                      \
+        mii_write(&ETHD1, 0x1F, 0xF100);                                \
+        valid &= mii_read(&ETHD1, 0x1F) == 0xF100;                      \
+        valid &= mii_read(&ETHD1, 0x00) == 0x40B3;                      \
+      }                                                                 \
+      if (valid) {                                                      \
+        mii_write(&ETHD1, 0x1F, 0xF410);                                \
+        valid &= mii_read(&ETHD1, 0x1F) == 0xF410;                      \
+        valid &= mii_read(&ETHD1, 0x00) == 0x2A05;                      \
+      }                                                                 \
+    } while (!valid);                                                   \
+                                                                        \
+    palSetLine(LINE_HEARTBEAT_LED_RED);                                 \
+                                                                        \
+    WRITE_REG(RCC->RSR, RCC_RSR_RMVF);                                  \
   } while (0)
 
 #define BOARD_HAS_RGB_STATUS 1
